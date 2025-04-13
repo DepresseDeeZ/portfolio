4f9fa4c4ef93e5647302b7d40177f6faf0d2985e
Umang Raval <132985504+DepresseDeeZ@users.noreply.github.com>
2025-04-13 22:26:14 +0530

    porfolio done

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import LogoSection from "./sections/LogoSection";
+import NavBar from "./components/NavBar";
+import FeatureCard from "./sections/FeatureCard";
+import Hero from "./sections/Hero";
+import ShowCaseSection from "./sections/ShowCaseSection";
+import ExperienceSection from "./sections/ExperienceSection";
+import TechStack from "./sections/TechStack";
+import Testimonalis from "./sections/Testimonalis";
+import Contact from "./sections/Contact";
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <ShowCaseSection />
+      {/* <LogoSection /> */}
+      <FeatureCard />
+      <ExperienceSection />
+      <TechStack />
+      <Testimonalis />
+      <Contact />
+    </>
+  );
+};
+
+export default App;
